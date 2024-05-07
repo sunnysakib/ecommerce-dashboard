95a76dad09ac8cad09cf264599dab5d4fc0c999d
sunnysakib <sakibur.rahaman033@gmail.com>
2024-05-07 10:37:58 +0600

    initialize project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline text-red-600'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
